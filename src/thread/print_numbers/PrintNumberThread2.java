4d10cb313cef726589988e79e748352882de12ff
adamhand <adaihand@163.com>
2019-09-02 20:23:58 +0800

    add

--- src/thread/print_numbers/PrintNumberThread2.java
+++ src/thread/print_numbers/PrintNumberThread2.java
@@ -2,32 +2,26 @@ package thread.print_numbers;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-//两个线程交替打印奇偶数，无锁，还有问题
+//两个线程交替打印奇偶数，无锁。
 public class PrintNumberThread2 implements Runnable{
     private static AtomicInteger num = new AtomicInteger();
-    private boolean flag;
-
-    PrintNumberThread2(boolean flag) {
-        this.flag = flag;
-    }
+    private static boolean flag = false;
 
     @Override
     public void run() {
         while (num.intValue() < 10) {
             if (flag) {
-                if ((num.intValue() & 0x01) == 0) {
-                    System.out.println(Thread.currentThread().getName() + " " + num.getAndIncrement());
-                }
+                System.out.println(Thread.currentThread().getName() + " " + num.getAndIncrement());
+                flag = false;
             } else {
-                if ((num.intValue() & 0x01) == 1) {
-                    System.out.println(Thread.currentThread().getName() + " " + num.getAndIncrement());
-                }
+                System.out.println(Thread.currentThread().getName() + " " + num.getAndIncrement());
+                flag = true;
             }
         }
     }
 
     public static void main(String[] args) {
-        new Thread(new PrintNumberThread2(true)).start();
-        new Thread(new PrintNumberThread2(false)).start();
+        new Thread(new PrintNumberThread2()).start();
+        new Thread(new PrintNumberThread2()).start();
     }
 }
